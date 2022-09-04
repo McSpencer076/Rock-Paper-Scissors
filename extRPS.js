f5a0c825b74378f33202558e85a3d7f1d4eadecb
McSpencer076 <merijn2@gmail.com>
2022-09-04 12:46:40 +0200

    added buttons and a scoreboard

--- extRPS.js
+++ extRPS.js
@@ -1,9 +1,76 @@
-const computerInput = computerPlay();
-const playerInput = playerPlay();
-
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
+let resultMessage = "This is the first round!"
+let finalResultMessage = ""
+
+const contentPlayer = document.createElement('div');
+contentPlayer.classList.add('contentPlayer');
+contentPlayer.textContent = playerScore;
+
+const contentComputer = document.createElement('div');
+contentComputer.classList.add('contentComputer');
+contentComputer.textContent = computerScore;
+
+const contentRound = document.createElement('div');
+contentRound.classList.add('contentRound');
+contentRound.textContent = roundNumber;
+
+const contentFinalScore = document.createElement('div');
+contentFinalScore.classList.add('contentFinalScore');
+contentFinalScore.textContent = ""
+
+results.appendChild(contentPlayer);
+results.appendChild(contentComputer);
+results.appendChild(contentRound);
+results.appendChild(contentFinalScore);
+
+const contentMessage = document.createElement("div");
+contentMessage.classList.add("contentMessage");
+contentMessage.textContent = resultMessage
+
+messages.appendChild(contentMessage);
+
+const buttons = document.querySelectorAll("button.gameButton")
+buttons.forEach((button) => {
+    button.addEventListener("click", function(e){
+        btnValue = button.id;
+        if(computerScore <= 4 && playerScore <= 4){
+        gameRound(computerPlay(), btnValue);
+        }else if(computerScore >= 5 || playerScore >= 5){
+            if(playerScore > computerScore){
+                finalResultMessage = "You Won! Great Job!"
+            }else if(computerScore > playerScore){
+                finalResultMessage = "Computer Won! Better luck next time!"                
+            }
+        
+        }
+        contentPlayer.textContent = playerScore
+        contentComputer.textContent = computerScore
+        contentRound.textContent = roundNumber
+        contentMessage.textContent = resultMessage;
+        contentFinalScore.textContent = finalResultMessage;
+        
+    })
+})
+
+const resetButton = document.querySelector("#resetButton")
+resetButton.addEventListener("click", function(e){
+    contentMessage.textContent = "The Game has been reset.";
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    finalResultMessage = ""
+    contentPlayer.textContent = playerScore
+    contentComputer.textContent = computerScore
+    contentRound.textContent = roundNumber
+    contentFinalScore.textContent = finalResultMessage;
+    
+
+});
+
+const computerInput = computerPlay();
+const playerInput = playerPlay();
 
 function computerPlay(){
     let randomNumber = Math.floor(Math.random() *3) +1 ;
@@ -16,31 +83,23 @@ function computerPlay(){
 };
 
 function playerPlay(){
-    let playerAction = prompt("Steen, papier, schaar?")
-    let playerChoice = playerAction.toLowerCase();
-    return playerChoice;
+    let playerAction = buttons;
+    return playerAction;
   }
 
 function gameRound(computerInput, playerInput){
     if (computerInput === playerInput){
-        console.log("ITS A TIE!")
+        resultMessage = "ITS A TIE!"
         ++roundNumber
     } else if (computerInput === "steen" && playerInput === "schaar" || computerInput === "papier" && playerInput === "steen" || computerInput === "schaar" && playerInput === "papier"){
-        console.log("Computer Wins! " + computerInput + " beats " + playerInput)
+        resultMessage = "Computer Wins! " + computerInput + " beats " + playerInput
         ++roundNumber
         ++computerScore
     } else if (computerInput === "steen" && playerInput === "papier" || computerInput === "papier" && playerInput === "schaar" || computerInput === "schaar" && playerInput === "steen"){
-        console.log("Player Wins! " + computerInput + " beats " + playerInput) 
+        resultMessage = "Player Wins! " + computerInput + " beats " + playerInput
         ++roundNumber
         ++playerScore
     } else {
-        console.log("WRONG INPUT!")
+        resultMessage = "WRONG INPUT!"
     }
-    console.log(roundNumber);
-    console.log(playerScore);
-    console.log(computerScore);
     }
-    
-    for (let i = 0; i < 5; i++) {
-    gameRound(computerPlay(), playerPlay());
-    } 
